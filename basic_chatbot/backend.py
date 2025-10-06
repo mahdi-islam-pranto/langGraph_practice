2ac7f3a066c10870600a8e77a015e6ac14a451a0
mahdi-islam-pranto <mahdiprantoblog@gmail.com>
2025-10-06 17:36:30 +0600

    basic chatbot with streamlit frontend

---
+++ basic_chatbot/backend.py
@@ -0,0 +1,48 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import BaseMessage, SystemMessage, HumanMessage, AIMessage
+# for add all messages to state
+from langgraph.graph.message import add_messages
+# for memory saver (local ram)
+from langgraph.checkpoint.memory import MemorySaver
+from dotenv import load_dotenv
+# load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4o-mini", api_key="")
+# define state
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+# define functions
+def chat_node(state: ChatState):
+    all_messages = state['messages']
+    llm_response = llm.invoke(all_messages)
+    return {'messages': [llm_response]}
+
+
+# define memory
+check_pointer = MemorySaver()
+
+# define state
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+
+chatbot = graph.compile(checkpointer=check_pointer)
+
+thread_id = "1"
+
+config = {'configurable': {"thread_id": thread_id}}
+
+# make the loop for the chatbot
+
+
+   
+
+# initial_state = {'messages': [SystemMessage(content="You are a helpful chatbot."),
+#                                   HumanMessage(content=user_input)]}
+# response = chatbot.invoke(initial_state, config=config)
+# print("Chatbot: " + response['messages'][-1].content)
---
+++ basic_chatbot/streamlit_frontend.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from backend import chatbot
+from langchain_core.messages import HumanMessage, SystemMessage
+
+st.title("Ask me anything")
+
+if "message_history" not in st.session_state:
+    st.session_state["message_history"] = []
+
+# sample message history
+# st.session_state["message_history"] = [
+#     {"role": "user", "content": "Hello"},
+#     {"role": "assistant", "content": "Hello, how can I help you?"},
+# ]
+
+# get user input
+user_input = st.chat_input("Type here: ")
+
+# show message history in the chat interface
+for message in st.session_state["message_history"]:
+    with st.chat_message(message["role"]):
+        st.text(message["content"])
+
+# show user input in the chat interface
+if user_input:
+    st.session_state["message_history"].append({"role": "user", "content": user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+
+    thread_id = "1"
+
+    config = {'configurable': {"thread_id": thread_id}}
+
+    # get chatbot response
+    response = chatbot.invoke({"messages": [SystemMessage(content="You are a helpful chatbot."),
+                                  HumanMessage(content=user_input)]}, config=config )
+    ai_response = response['messages'][-1].content
+    # put ai response in the message history
+    st.session_state["message_history"].append({"role": "assistant", "content": ai_response})
+    # show chatbot response in the chat interface
+    with st.chat_message('assistant'):
+        st.text(ai_response)
