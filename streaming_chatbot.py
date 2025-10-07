7df94f6eefa76f339f9cf4b53d41f1cfa4906293
mahdi-islam-pranto <mahdiprantoblog@gmail.com>
2025-10-07 13:30:15 +0600

    streaming feature in AI response

--- streaming_chatbot.py
+++ streaming_chatbot.py
@@ -7,10 +7,10 @@ from langgraph.graph.message import add_messages
 # for memory saver (local ram)
 from langgraph.checkpoint.memory import MemorySaver
 from dotenv import load_dotenv
-# load_dotenv()
+load_dotenv()
 
+# define llm
 llm = ChatOpenAI(model="gpt-4o-mini", api_key="")
-
 # define state
 class ChatState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
@@ -21,7 +21,6 @@ def chat_node(state: ChatState):
     llm_response = llm.invoke(all_messages)
     return {'messages': [llm_response]}
 
-
 # define memory
 check_pointer = MemorySaver()
 
@@ -31,7 +30,7 @@ graph.add_node("chat_node", chat_node)
 graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
-
+# compile
 chatbot = graph.compile(checkpointer=check_pointer)
 
 thread_id = "1"
@@ -40,10 +39,18 @@ config = {'configurable': {"thread_id": thread_id}}
 
 # make the loop for the chatbot
 
+user_input = "write a very short twitter post about AI"
+# print("User: " + user_input)
 
-   
+initial_state = {'messages': [SystemMessage(content="You are a helpful twitter chatbot."),
+                              HumanMessage(content=user_input)]}
 
-# initial_state = {'messages': [SystemMessage(content="You are a helpful chatbot."),
-#                                   HumanMessage(content=user_input)]}
+# get llm response as stream
+for message_chunk in chatbot.stream(initial_state, config=config, stream_mode="messages"):
+    if message_chunk:
+        print(message_chunk, end=" ", flush=True)
+    
+    # print("Chatbot: " + message_chunk.content, end=" ", flush=True)
+
 # response = chatbot.invoke(initial_state, config=config)
 # print("Chatbot: " + response['messages'][-1].content)
